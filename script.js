2f6c8d259e49abf137a14a38414369eaaae321f9
Sameer Shaik <87945863+sameershaik-sudo@users.noreply.github.com>
2025-04-24 22:37:48 +0530

    added account deletion

--- script.js
+++ script.js
@@ -180,3 +180,23 @@ btnTransfer.addEventListener("click", function (e) {
   // Update UI
   updateUI(currentAccount);
 });
+
+btnClose.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  if (
+    inputCloseUsername.value === currentAccount.username &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    const index = accounts.findIndex(
+      (acc) => acc.username === currentAccount.username
+    );
+
+    //delete account
+    accounts.splice(index, 1);
+
+    //hide UI
+    containerApp.style.opacity = 0;
+  }
+  inputCloseUsername.value = inputClosePin.value = "";
+});
