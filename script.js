cdb434e527e5a6b7845e39f58090ef5d6e652688
Sameer Shaik <87945863+sameershaik-sudo@users.noreply.github.com>
2025-04-26 19:35:44 +0530

    added loan taking functionality

--- script.js
+++ script.js
@@ -156,6 +156,22 @@ btnLogin.addEventListener("click", function (e) {
   }
 });
 
+btnLoan.addEventListener("click", function (e) {
+  e.preventDefault();
+  const amount = Number(inputLoanAmount.value);
+  if (
+    amount > 0 &&
+    currentAccount.movements.some((mov) => mov >= 0.1 * amount) // some is used when any one of the elements satisfies the condition
+  ) {
+    // add movement
+    currentAccount.movements.push(amount);
+
+    // update UI
+    updateUI(currentAccount);
+  }
+  inputLoanAmount.value = "";
+});
+
 // Transfers
 btnTransfer.addEventListener("click", function (e) {
   e.preventDefault();
